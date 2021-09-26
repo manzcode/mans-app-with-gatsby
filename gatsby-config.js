210ac51931b208bdb1ecf149a29f8026bbec6ffd
mariomans <mariomans89@gmail.com>
2021-09-26 17:36:18 +0300

    end of gatsby tutorile part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,9 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "mans_site",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    // `gatsby-plugin-image`,
+    // `gatsby-plugin-sharp`
+  ]
 };
